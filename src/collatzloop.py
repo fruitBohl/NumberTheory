cc550a6b80be0ee959dd1497455d202b18093db9
Harrison Maxwell Bohl <bohlharrison357@gmail.com>
2023-03-04 21:56:30 +1000

    feat: add src directory and start first cut.

---
+++ src/collatzloop.py
@@ -0,0 +1,21 @@
+def determine_congruence_relations(depth: int):
+   """ Recursive function which determines congruence relations of elements
+       within a
+   """
+
+   print(f"depth = {depth}")
+
+   if depth == 0:
+      return True
+   else:
+      return determine_congruence_relations(depth-1)
+
+if __name__ == "__main__":
+   print("""
+      Assuming a loop exists this code will recursively determine congruence
+      relations of elements within the loop (starting from the maximum element).
+      This code uses the fact that every element within the loop must be 
+      divisible by three and the maximum element of the loop is 4 (mod 12)
+   """)
+
+   determine_congruence_relations(10)
