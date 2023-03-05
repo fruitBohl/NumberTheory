896e408c212aba7e0b2111634798520434aea6a2
Harrison Maxwell Bohl <bohlharrison357@gmail.com>
2023-03-05 11:46:31 +1000

    feat: adding basics

--- src/collatzloop.py
+++ src/collatzloop.py
@@ -1,21 +1,28 @@
+
+import math as m
+import pandas as pd
+import numpy as np
+
+
 def determine_congruence_relations(depth: int):
-   """ Recursive function which determines congruence relations of elements
-       within a
-   """
+    """Recursive function which determines congruence relations of elements
+    within a loop."""
 
-   print(f"depth = {depth}")
+    print(f"depth = {depth}")
 
-   if depth == 0:
-      return True
-   else:
-      return determine_congruence_relations(depth-1)
+    if depth == 0:
+        return True
+    else:
+        return determine_congruence_relations(depth - 1)
+
 
 if __name__ == "__main__":
-   print("""
-      Assuming a loop exists this code will recursively determine congruence
-      relations of elements within the loop (starting from the maximum element).
-      This code uses the fact that every element within the loop must be 
-      divisible by three and the maximum element of the loop is 4 (mod 12)
-   """)
+    print(
+        """Assuming a loop exists this code will recursively determine 
+        congruence relations of elements within the loop (starting from the 
+        maximum element). This code uses the fact that every element within 
+        the loop must be divisible by three and the maximum element of the 
+        loop is 4 (mod 12)"""
+    )
 
-   determine_congruence_relations(10)
+    determine_congruence_relations(10)
