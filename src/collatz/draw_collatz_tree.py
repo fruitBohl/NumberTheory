bfac26a1d84c3e5a1791e9aff4c0087c2204a743
Harrison Bohl <bohlharrison357@gmail.com>
2023-03-07 22:00:57 +1000

    feat: almost working, just need to solve case when 4 elements are generated

--- src/collatz/draw_collatz_tree.py
+++ src/collatz/draw_collatz_tree.py
@@ -4,15 +4,15 @@ from binarytree import build, tree
 def collatz(
     paths: list[list],
     iterations_remaining: int,
-    num_prev_generated_elems: int,
+    prev_generated_elems: list[int],
     num_iterations: int,
 ) -> list[int]:
     if iterations_remaining > 0:
-        paths, num_prev_generated_elems = add_to_paths(
-            paths, num_prev_generated_elems, num_iterations, iterations_remaining
+        paths, prev_generated_elems = add_to_paths(
+            paths, prev_generated_elems, num_iterations, iterations_remaining
         )
         return collatz(
-            paths, iterations_remaining - 1, num_prev_generated_elems, num_iterations
+            paths, iterations_remaining - 1, prev_generated_elems, num_iterations
         )
     elif iterations_remaining == 0:
         return paths
@@ -20,6 +20,7 @@ def collatz(
 
 def add_nones(paths: list[list[int]], num: int) -> list[list[int]]:
     num_nones = 0
+    if num < 0: num =0
 
     while num_nones != num:
         paths.append(None)
@@ -29,66 +30,72 @@ def add_nones(paths: list[list[int]], num: int) -> list[list[int]]:
 
 def add_to_paths(
     paths: list[int],
-    num_prev_generated_elems: int,
+    prev_generated_elems: list[int],
     num_iterations: int,
     iterations_remaining: int,
-) -> tuple[list[int], int]:
+) -> tuple[list[int], list[int]]:
     elements_searched = 0
     index = 0
     num_generated_elems = 0
+    elements_generated = []
 
-    reverse_paths = paths.copy()
-    reverse_paths.reverse()
+    for count, element in enumerate(prev_generated_elems):
+        element_index = paths.index(element)
+        if count == 0:
+            paths = add_nones(paths, 2**(num_iterations - iterations_remaining)-1)
+        paths.append(element * 2)
+        elements_generated.append(element* 2)
 
-    print("reverse paths = ", reverse_paths)
-    print(
-        "number of iterations - iterations_remaining = ",
-        (num_iterations - iterations_remaining),
-    )
-    print("num prev gen elems = ", num_prev_generated_elems)
+        if (element % 6 == 4) & (element > 4):
+            # paths.append((int ((element-1)/3)))
+            if count != 0:
+                paths.append((int ((element-1)/3)))
+            elif (element_index+2**(num_iterations-iterations_remaining)+1) < len(paths):
+                paths[element_index+2**(num_iterations-iterations_remaining)+1] = (int ((element-1)/3))
+            else:
+                paths.append(int ((element-1)/3))
+            # elif (element_index+2**(num_iterations-iterations_remaining)+1) < len(paths):
+            #     paths[element_index+2**(num_iterations-iterations_remaining)+1]=(int ((element-1)/3))
+            elements_generated.append(int ((element-1)/3))
 
-    if len(paths) == 1:
-        previously_generated_elements = [1]
-    elif len(paths) == 3:
-        previous_generated_elements = [2,1]
-    else:
-        previously_generated_elements = reverse_paths[
-            (num_iterations - iterations_remaining) ** 2 :
-        ]
+    # reverse_paths = paths.copy()
+    # reverse_paths.reverse()
 
-    print("prev gen elems = ", previously_generated_elements)
+    # while elements_searched < len(prev_generated_elems):
+    #     if reverse_paths[index] == None:
+    #         index += 1
+    #         continue
+
+    #     if len(prev_generated_elems) > 1:
+    #         paths = add_nones(paths, len(prev_generated_elems)**2 - 2)
+    #     elements_generated.append(reverse_paths[index] * 2)
+    #     paths.append(reverse_paths[index] * 2)
+
+    #     if (reverse_paths[index] % 6 == 4) & (reverse_paths[index] > 4):
+    #         paths.append(int((reverse_paths[index] - 1) / 3))
+    #         elements_generated.append(int((reverse_paths[index] - 1) / 3))
+    #         paths = add_nones(
+    #             paths, 2 ** (num_iterations - iterations_remaining + 1) - 2
+    #         )
+    #     else:
+    #         paths = add_nones(
+    #             paths, 2 ** (num_iterations - iterations_remaining + 1) - 1
+    #         )
+    #     index += 1
+    #     elements_searched += 1
+
+    # print("elements generated", elements_generated)
+
+    print("paths", paths)
+    print("elements generated", elements_generated)
     print("")
 
-    while elements_searched < num_prev_generated_elems:
-        if reverse_paths[index] == None:
-            index += 1
-            continue
-
-        if num_prev_generated_elems > 1:
-            paths = add_nones(paths, num_prev_generated_elems**2 - 2)
-            paths.append(reverse_paths[index] * 2)
-        else:
-            paths.append(reverse_paths[index] * 2)
-
-        if (reverse_paths[index] % 6 == 4) & (reverse_paths[index] > 4):
-            paths.append(int((reverse_paths[index] - 1) / 3))
-            num_generated_elems += 1
-            paths = add_nones(
-                paths, 2 ** (num_iterations - iterations_remaining + 1) - 2
-            )
-        else:
-            paths = add_nones(
-                paths, 2 ** (num_iterations - iterations_remaining + 1) - 1
-            )
-        index += 1
-        elements_searched += 1
-        num_generated_elems += 1
-
-    return paths, num_generated_elems
+    return paths, elements_generated
 
 
 def main():
-    paths = collatz([1], 6, 1, 6)
+    paths = collatz([1], 7, [1], 7)
+    print(paths)
     tree = build(paths)
     print(tree)
 
