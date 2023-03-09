46f875dbe3daeca278b1a414333817e6fcc7a587
Harrison Bohl <bohlharrison357@gmail.com>
2023-03-09 19:32:57 +1000

    chore: run auto-formatting over all files

--- src/collatz/evens_vs_odds.py
+++ src/collatz/evens_vs_odds.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 
+
 def collatz(
     paths: list[tuple[list[int], int]], iterations_remaining: int
 ) -> list[tuple[list[int], int]]:
     if iterations_remaining == 0:
-        min_value = 2^63 - 1
+        min_value = 2 ^ 63 - 1
         min_path = []
         data = {}
         current_path = 1
 
-        for (path, evens) in paths:
-            data[f'path {current_path}'] = evens/51
+        for path, evens in paths:
+            data[f"path {current_path}"] = evens / 51
             current_path += 1
 
             if evens <= min_value:
@@ -26,20 +27,20 @@ def collatz(
     else:
         paths = add_to_paths(paths)
         collatz(paths, iterations_remaining - 1)
-        
+
 
 def add_to_paths(paths: list[tuple[list[int], int]]) -> list[tuple[list[int], int]]:
     new_paths = []
 
-    for (path, evens) in paths:
+    for path, evens in paths:
         endpoint = path[-1]
         new_path = path.copy()
         other_path = path.copy()
         new_path.append(endpoint * 2)
-        
-        if (new_path, evens+1) not in new_paths:
-            new_paths.append((new_path, evens+1))
-        
+
+        if (new_path, evens + 1) not in new_paths:
+            new_paths.append((new_path, evens + 1))
+
         if (endpoint % 3 == 1) & (endpoint > 4) & (endpoint % 2 == 0):
             new_evens = evens
 
@@ -47,14 +48,16 @@ def add_to_paths(paths: list[tuple[list[int], int]]) -> list[tuple[list[int], in
                 new_evens += 1
 
             other_path.append(int((endpoint - 1) / 3))
-            
+
             if (other_path, new_evens) not in new_paths:
                 new_paths.append((other_path, new_evens))
 
     return new_paths
 
+
 def main():
     collatz([([1], 0)], 51)
+
 
 if __name__ == "__main__":
     main()
--- src/collatz/misc/collatz_vince_1.py
+++ src/collatz/misc/collatz_vince_1.py
@@ -9,16 +9,19 @@ Created on Fri Feb 17 22:58:19 2023
 # estimating the expected proportion of evens in a collatz sequence of length n
 # will print this proportion
 
-#sequence length 
-seq_len = 30 #this is the variable that you can change
+# sequence length
+seq_len = 30  # this is the variable that you can change
+
 
 def recursive(possible_paths: list[tuple[list[int], int]], sequence_length: int):
     if sequence_length == 1:
         exp_num_evens = []
         for path in possible_paths:
-            exp_num_evens.append((seq_len - sum(path[0]))*path[1])
-        print(f'Expected proportion of primes in all sequences of length {seq_len} = {sum(exp_num_evens)/seq_len}')
-            
+            exp_num_evens.append((seq_len - sum(path[0])) * path[1])
+        print(
+            f"Expected proportion of primes in all sequences of length {seq_len} = {sum(exp_num_evens)/seq_len}"
+        )
+
     else:
         possible_paths = iterate(possible_paths)
         recursive(possible_paths, sequence_length - 1)
@@ -26,36 +29,35 @@ def recursive(possible_paths: list[tuple[list[int], int]], sequence_length: int)
 
 def iterate(possible_paths: list[tuple[list[int], int]]):
     all_paths = []
-    for (path, likelihood) in possible_paths:
-        
+    for path, likelihood in possible_paths:
         odd_even_path = path.copy()
         odd_even_likelihood = likelihood
-        
+
         even_odd_path = path.copy()
         even_odd_likelihood = likelihood
 
         even_even_path = path.copy()
         even_even_likelihood = likelihood
 
-        
         if path[-1] == 1:
             odd_even_path.append(0)
             all_paths.append((odd_even_path, odd_even_likelihood))
-            
+
         else:
             even_odd_path.append(1)
-            even_odd_likelihood *= 1/3
+            even_odd_likelihood *= 1 / 3
             all_paths.append((even_odd_path, even_odd_likelihood))
-            
+
             even_even_path.append(0)
-            even_even_likelihood *= 2/3
+            even_even_likelihood *= 2 / 3
             all_paths.append((even_even_path, even_even_likelihood))
-            
-    return(all_paths)
-            
+
+    return all_paths
+
+
 def main():
     recursive([([1], 1)], seq_len)
-    
+
+
 if __name__ == "__main__":
     main()
-            
