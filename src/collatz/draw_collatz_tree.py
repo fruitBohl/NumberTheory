cecd878a6ec67d4687e3c06bb82a76e78c41698c
Harrison Bohl <bohlharrison357@gmail.com>
2023-03-09 17:26:52 +1000

    feat: completed collatz visualisation

--- src/collatz/draw_collatz_tree.py
+++ src/collatz/draw_collatz_tree.py
@@ -9,7 +9,7 @@ def collatz(
 ) -> list[int]:
     if iterations_remaining > 0:
         paths, prev_generated_elems = add_to_paths(
-            paths, prev_generated_elems, num_iterations, iterations_remaining
+            paths, prev_generated_elems, num_iterations
         )
         return collatz(
             paths, iterations_remaining - 1, prev_generated_elems, num_iterations
@@ -33,44 +33,31 @@ def add_to_paths(
     paths: list[int],
     prev_generated_elems: list[int],
     num_iterations: int,
-    iterations_remaining: int,
 ) -> tuple[list[int], list[int]]:
     elements_generated = []
 
     paths = add_nones(paths, 2**num_iterations)
 
-    for count, element in enumerate(prev_generated_elems):
+    for element in prev_generated_elems:
         element_index = paths.index(element)
-
         elements_generated.append(element * 2)
-        paths[element_index + 2 ** (num_iterations - iterations_remaining) + count] = (
-            element * 2
-        )
+
+        if element_index == 0:
+            paths[1] = element * 2
+        elif element_index == 1:
+            paths[3] = element * 2
+        else:
+            paths[element_index * 2 + 1] = element * 2 
 
         if (element % 6 == 4) & (element > 4):
-            distance_from_prev_elem = 0
-            if count > 0:
-                distance_from_prev_elem = element_index - paths.index(
-                    prev_generated_elems[count - 1]
-                )
-
             elements_generated.append(int((element - 1) / 3))
-            paths[
-                element_index
-                + 2 ** (num_iterations - iterations_remaining)
-                + (2 * distance_from_prev_elem)
-                + 1
-            ] = int((element - 1) / 3)
-
-    print("paths", paths)
-    print("elements generated", elements_generated)
-    print("")
+            paths[element_index * 2 + 2] = int((element - 1) / 3)
 
     return paths, elements_generated
 
 
 def main():
-    paths = collatz([1], 8, [1], 8)
+    paths = collatz([1], 9, [1], 9)
     tree = build(paths)
     print(tree)
 
