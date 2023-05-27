06ebe22e752021ced8b61a03844603495af55cda
Harrison Bohl <bohlharrison357@gmail.com>
2023-05-27 15:48:25 +1000

    feat: starting graph theory investigation

---
+++ src/expander_graphs/calculate_cheeger_constant.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from typing import *
+
+
+def calculate_cheeger_constant(G) -> int:
+    return None
+
+
+if __name__ == "__main__":
+    # G = nx.random_regular_graph(3, 6)
+    # G = nx.petersen_graph()
+    # G = nx.erdos_renyi_graph(10,0.4)
+    G = nx.random_lobster(10,0.6,0.2)
+    nx.draw(G)
+    plt.savefig("graph.pdf")
