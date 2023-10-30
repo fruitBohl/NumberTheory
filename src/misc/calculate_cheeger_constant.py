347fbc5f750e80041662810b141788a75b5b46a2
Harrison Bohl <bohlharrison357@gmail.com>
2023-10-31 01:21:53 +1000

    feat: calculate cheeger constant with LP

--- src/misc/calculate_cheeger_constant.py
+++ src/misc/calculate_cheeger_constant.py
@@ -3,14 +3,41 @@ import matplotlib.pyplot as plt
 from typing import *
 
 
-def calculate_cheeger_constant(G) -> int:
-    return None
+def calculate_cheeger_constant() -> int:
+    # Sets
+    num_nodes = 5
+
+    V = range(5)  # set of nodes
+    E = [frozenset({v, (v + 1) % 5}) for v in V]  # set of edges
+    G = [
+        frozenset({v, (v + 1) % 5}) for v in V
+    ]  # adjacency list representation of graph
+
+    # A path is a sorted tuple of squares visited, plus the square we are up to
+    DA = [{(frozenset({v}), v) for v in V}]
+    for l in V[1:]:
+        DA.append(set())
+        for d in DA[l - 1]:
+            for v in G(d[1]):
+                if v not in d[0]:
+                    DA[l].add((d[0] | {v}, v))
+
+    P = set(p[0] for p in DA[V[-1]])
+
+    Paths = [{(frozenset({s}), s) for s in S}]
+    for t in T[1:]:
+        Paths.append(set())
+        for p in Paths[t - 1]:
+            for s in Neighbours(p[1], nMax):
+                if s not in p[0]:
+                    Paths[t].add((p[0] | {s}, s))
+
+    # Variables
+
+    # Objective
+
+    return
 
 
 if __name__ == "__main__":
-    # G = nx.random_regular_graph(3, 6)
-    # G = nx.petersen_graph()
-    # G = nx.erdos_renyi_graph(10,0.4)
-    G = nx.random_lobster(10, 0.6, 0.2)
-    nx.draw(G)
-    plt.savefig("graph.pdf")
+    calculate_cheeger_constant
--- src/twinprime/dist_prime_gaps_not_consec.py
+++ src/twinprime/dist_prime_gaps_not_consec.py
@@ -1,16 +1,17 @@
-#%%Distribution of not necessarily consecutive prime gaps 
+# %%Distribution of not necessarily consecutive prime gaps
 
 #
-#import math
+# import math
 import pylab
 from matplotlib import pyplot as plt
 
 
 # CONSTANTS
-M = 20000 #number of primes examined. 
-#maybe need to download a large file of prime numbers. 
+M = 20000  # number of primes examined.
+# maybe need to download a large file of prime numbers.
 
 # FUNCTIONS
+
 
 def prime_numbers(pn: int):
     p = []
@@ -22,38 +23,51 @@ def prime_numbers(pn: int):
             p.append(i)
     return p
 
-def primorials(pn: int): 
+
+def primorials(pn: int):
     p = prime_numbers(pn)
     q = [2]
     for i in p[1:]:
         if max(q) < max(p):
-            q.append(i*max(q))
-    return(q)
+            q.append(i * max(q))
+    return q
+
 
 primes_ = prime_numbers(M)
 primos = primorials(M)
 
-GAPS = {i:[] for i in primos}
-for N in primes_: 
-    GAPS_ = {i:0 for i in primos}
+GAPS = {i: [] for i in primos}
+for N in primes_:
+    GAPS_ = {i: 0 for i in primos}
     primes = prime_numbers(N)
-    for i in primes: 
-        for j in primes[primes.index(i)+1:]:
-            if j-i in primos:
-                GAPS_[j-i] += 1
+    for i in primes:
+        for j in primes[primes.index(i) + 1 :]:
+            if j - i in primos:
+                GAPS_[j - i] += 1
     for i in GAPS_:
         GAPS[i].append(GAPS_[i])
 
-RATS = {i:[] for i in primos}
-for i in range(len(primes)): 
+RATS = {i: [] for i in primos}
+for i in range(len(primes)):
     for gap in GAPS:
         if GAPS[gap][i] == 0:
             RATS[gap].append(0)
         else:
-            RATS[gap].append(GAPS[2][i]/GAPS[gap][i])
+            RATS[gap].append(GAPS[2][i] / GAPS[gap][i])
 
 
-COLS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+COLS = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+]
 
 # for gap in GAPS:
 #     col = COLS[list(GAPS.keys()).index(gap)]
