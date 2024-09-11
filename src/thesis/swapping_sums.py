742dc138d61951d6f770dd263fb7f825697786af
Harrison Bohl <bohlharrison357@gmail.com>
2024-09-11 14:55:50 +1000

    feat: improvements

--- src/thesis/swapping_sums.py
+++ src/thesis/swapping_sums.py
@@ -1,10 +1,5 @@
-from __future__ import annotations
-from math import cos, sqrt, pi, comb, ceil, floor, prod, gcd
+from math import floor
 from itertools import combinations
-from sympy import isprime
-from sympy.ntheory import factorint
-import networkx as nx
-import matplotlib.pyplot as plt
 from sympy.ntheory.factor_ import totient
 
 
@@ -27,8 +22,6 @@ def mobius(N):
             if N % (i * i) == 0:
                 return 0
             else:
-                # i occurs only once,
-                # increase f
                 p = p + 1
 
     if p % 2 != 0:
@@ -59,7 +52,7 @@ def sum_phi_diff2(n: int) -> int:
 
     phi_sum = sum(
         [
-            mobius(m) * sum(d for d in [i for i in range(1, floor(n / m)+1)])
+            mobius(m) * sum(d for d in [i for i in range(1, floor(n / m) + 1)])
             for m in range(1, n + 1)
         ]
     )
@@ -68,4 +61,6 @@ def sum_phi_diff2(n: int) -> int:
 
 
 if __name__ == "__main__":
-    print(sum_phi_std(10), sum_phi_diff1(10), sum_phi_diff2(10))
+    for n in range(3, 100):
+        assert sum_phi_std(n) == sum_phi_diff1(n) == sum_phi_diff2(n)
+    print("True")
